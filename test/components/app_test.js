a3d910714c4d7f10d8a9c2fe6a8ae089a032be25
Nick Nauert <nicknauert@gmail.com>
2017-11-21 13:47:32 -0500

    testing begins

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,1 +1,14 @@
+import { renderComponent, expect } from "../test_helper"
+import App from "../../src/components/app";
 
+
+// Use 'describe' to group together similar tests
+// This string is only for the report
+describe('App')
+
+// Use 'it' to test a single attribute of a target
+// This string is only for the report
+it('shows the correct test')
+
+// Use 'expect' to make an assertion about a target
+expect
