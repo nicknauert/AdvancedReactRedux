782dc9c64e4c6251dcaf0eccdf1ed6ed075f1333
Nick Nauert <nicknauert@gmail.com>
2017-11-25 15:13:45 -0500

    actions wired up via nify shortcut

--- src/components/commentBox.js
+++ src/components/commentBox.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component{
+class CommentBox extends Component{
     constructor(props){
         super(props);
         this.state = { comment: '' };
@@ -12,6 +14,7 @@ export default class CommentBox extends Component{
 
     handleSubmit(e){
         e.preventDefault();
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     }
 
@@ -26,3 +29,5 @@ export default class CommentBox extends Component{
         );
     }
 }
+
+export default connect(null, actions)(CommentBox)
