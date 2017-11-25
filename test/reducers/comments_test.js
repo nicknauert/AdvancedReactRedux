bdf7fa289a0f0ecf01996c41da58796b71f42b04
Nick Nauert <nicknauert@gmail.com>
2017-11-25 15:19:12 -0500

    testing reducers

---
+++ test/reducers/comments_test.js
@@ -0,0 +1,12 @@
+import { expect } from '../test_helper';
+import commentReducer from '../../src/reducers/comments';
+import { SAVE_COMMENT } from '../../src/actions/types';
+
+describe('Comments Reducer', () => {
+    it('handles action with unknown type', () => {
+
+    });
+    it('SAVE_COMMENT', () => {
+
+    })
+});
