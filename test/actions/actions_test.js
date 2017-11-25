de966743811199cdb882c15aca0cc128cfb31e1c
Nick Nauert <nicknauert@gmail.com>
2017-11-25 15:01:15 -0500

    guessing expects

--- test/actions/actions_test.js
+++ test/actions/actions_test.js
@@ -1,13 +1,15 @@
 import { expect } from '../test_helper';
+import { SAVE_COMMENT } from '../../src/actions/types';
+import { saveComment } from '../../src/actions';
 
 describe('actions', () => {
     describe('saveComment', () => {
         it('has the correct type', () => {
-            
+            expect(SAVE_COMMENT).to.contain('save_comment');
         })
 
         it('has the correct payload', () => {
-            
+            expect(saveComment.payload).to.be('string');
         })
     });
 });
